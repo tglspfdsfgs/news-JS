a9ce6e12a84f6b954e1472e0e0218ec739849fba
tglspfdsfgs <tglspfdsfgs@gmail.com>
2023-04-12 19:20:26 +0300

    feat: add API key

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '46f9f4feaa0c497e8c39f581e758d120',
         });
     }
 }
