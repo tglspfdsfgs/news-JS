75122d7ccc5cdf6431a8b534c05c18602e88fae8
tglspfdsfgs <tglspfdsfgs@gmail.com>
2023-04-19 13:07:22 +0300

    feat: add types to app.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import App from './components/app/app';
+import { App, IApp } from './components/app/app';
 import './global.css';
 
-const app = new App();
+const app: IApp = new App();
 app.start();
