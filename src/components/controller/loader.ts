09f9093c59ba0a5b0e940fe482cccd17c25c4e6b
tglspfdsfgs <tglspfdsfgs@gmail.com>
2023-04-26 12:53:36 +0300

    feat: add types to Loader

--- src/components/controller/loader.ts
+++ src/components/controller/loader.ts
@@ -1,19 +1,31 @@
+import { ResponseCallback, ResObject } from '@interfaces';
+
+interface APIOptions {
+    [key: string]: string;
+}
+interface ReqOptions extends APIOptions {
+    apiKey: string;
+}
+
 class Loader {
-    constructor(baseLink, options) {
+    private readonly baseLink: string;
+    private readonly options: ReqOptions;
+
+    constructor(baseLink: string, options: ReqOptions) {
         this.baseLink = baseLink;
         this.options = options;
     }
 
     getResp(
-        { endpoint, options = {} },
-        callback = () => {
-            console.error('No callback for GET response');
+        { endpoint, options = {} }: { endpoint: string; options?: APIOptions },
+        callback: ResponseCallback = () => {
+            console.error('No callback for response');
         }
-    ) {
+    ): void {
         this.load('GET', endpoint, callback, options);
     }
 
-    errorHandler(res) {
+    private errorHandler(res: Response): Response {
         if (!res.ok) {
             if (res.status === 401 || res.status === 404)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
@@ -23,8 +35,8 @@ class Loader {
         return res;
     }
 
-    makeUrl(options, endpoint) {
-        const urlOptions = { ...this.options, ...options };
+    makeUrl(options: APIOptions, endpoint: string): string {
+        const urlOptions: ReqOptions = { ...this.options, ...options };
         let url = `${this.baseLink}${endpoint}?`;
 
         Object.keys(urlOptions).forEach((key) => {
@@ -34,11 +46,11 @@ class Loader {
         return url.slice(0, -1);
     }
 
-    load(method, endpoint, callback, options = {}) {
+    load(method: 'GET' | 'SET', endpoint: string, callback: ResponseCallback, options: APIOptions = {}): void {
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
             .then((res) => res.json())
-            .then((data) => callback(data))
+            .then((data: ResObject) => callback(data))
             .catch((err) => console.error(err));
     }
 }
