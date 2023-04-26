b4bab624c67142393c606a47b922b3fad7f99928
tglspfdsfgs <tglspfdsfgs@gmail.com>
2023-04-26 13:58:27 +0300

    feat: add types to AppView

--- src/components/view/appView.ts
+++ src/components/view/appView.ts
@@ -1,19 +1,24 @@
 import News from './news/news';
 import Sources from './sources/sources';
 
+import { Source, Arcticle, ResSrc, ResNews } from '@interfaces';
+
 export class AppView {
+    private news: News;
+    private sources: Sources;
+
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
-        const values = data?.articles ? data?.articles : [];
+    drawNews(data: ResNews) {
+        const values: Array<Arcticle> = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
-        const values = data?.sources ? data?.sources : [];
+    drawSources(data: ResSrc) {
+        const values: Array<Source> = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
 }
