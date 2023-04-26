c115d8c156ccc0ba0c723daf7a0c2ce1124a0447
tglspfdsfgs <tglspfdsfgs@gmail.com>
2023-04-26 12:44:15 +0300

    feat: add API types

---
+++ src/interfaces.ts
@@ -0,0 +1,31 @@
+export type Source = Readonly<{
+    id: string;
+    name: string;
+    description: string;
+    category: string;
+    language: string;
+    country: string;
+}>;
+
+export type Arcticle = Readonly<{
+    source: { id: string; name: string };
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string;
+    publishedAt: string;
+    content: string;
+}>;
+
+export type ResObject = Readonly<{
+    sources: Array<Source>;
+    articles: Array<Arcticle>;
+    status: 'ok' | 'error';
+    totalResults: number;
+}>;
+
+export type ResSrc = Pick<ResObject, 'status' | 'sources'>;
+export type ResNews = Pick<ResObject, 'status' | 'articles' | 'totalResults'>;
+
+export type ResponseCallback = (data: ResObject) => void;
